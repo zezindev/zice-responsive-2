591fe157a3befecbe69a69668bc34a03f6aca01f
José Lucas <joselucasskt@gmail.com>
2023-09-01 15:59:27 -0300

    Update

--- js/carousel.js
+++ js/carousel.js
@@ -1,33 +1,58 @@
-const innerDiv = document.querySelector(".inner-div")
-const leftArrow = document.querySelector(".left-arrow")
-const rightArrow = document.querySelector(".right-arrow")
-const dots = document.querySelectorAll(".dot")
+const images = document.querySelectorAll(".carousel img")
+const prevButton = document.getElementById("prev")
+const nextButton = document.getElementById("next")
+const dotsContainer = document.getElementById("dots")
 let currentIndex = 0
-const totalImages = 3
 
-function showImage(index) {
-  innerDiv.style.transform = `translateX(-${index * 380}px)`
+// Function to update the carousel display and pagination
+function updateCarousel() {
+  images.forEach((image, index) => {
+    if (index === currentIndex) {
+      image.style.display = "block"
+    } else {
+      image.style.display = "none"
+    }
+  })
 
-  dots.forEach((dot, i) => {
-    dot.classList.toggle("active-dot", i === index)
+  const dots = Array.from(dotsContainer.children)
+  dots.forEach((dot, index) => {
+    if (index === currentIndex) {
+      dot.classList.add("active")
+    } else {
+      dot.classList.remove("active")
+    }
   })
 }
 
-function navigate(direction) {
-  currentIndex = (currentIndex + direction + totalImages) % totalImages
-  showImage(currentIndex)
+// Function to handle the next button click
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % images.length
+  updateCarousel()
 }
 
-leftArrow.addEventListener("click", () => {
-  navigate(-1)
+// Function to handle the previous button click
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length
+  updateCarousel()
+}
+
+// Create dots for each image
+images.forEach((_, index) => {
+  const dot = document.createElement("span")
+  dot.classList.add("dot")
+  dot.addEventListener("click", () => {
+    currentIndex = index
+    updateCarousel()
+  })
+  dotsContainer.appendChild(dot)
 })
 
-rightArrow.addEventListener("click", () => {
-  navigate(1)
-})
+// Set initial display
+updateCarousel()
 
-setInterval(() => {
-  navigate(1)
-}, 3000)
+// Automatically advance the carousel every 3 seconds
+setInterval(nextSlide, 4000)
 
-showImage(currentIndex)
+// Add event listeners to the arrow buttons
+nextButton.addEventListener("click", nextSlide)
+prevButton.addEventListener("click", prevSlide)
