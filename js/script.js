db0dfe1066aaaea07ae2bf4f01fdf8c288980ff7
José Lucas <joselucasskt@gmail.com>
2023-09-13 17:10:50 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -18,4 +18,23 @@ chk.addEventListener("change", () => {
   }
 })
 
+//  mask
 $("#phone").mask("(000)-000-0000")
+
+//map
+
+var customIcon = L.icon({
+  iconUrl: "./imgs/map-marker.png", // Caminho correto para a sua imagem personalizada
+  iconSize: [32, 40], // Tamanho da imagem
+  iconAnchor: [16, 32], // Ponto de ancoragem do ícone (geralmente no centro da parte inferior)
+  popupAnchor: [0, -32], // Ponto de ancoragem do popup (acima do ícone)
+})
+
+var map = L.map("map").setView([45.504, -73.595], 14)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  // attribution:
+  //   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map)
+
+var marker = L.marker([45.504, -73.595], { icon: customIcon }).addTo(map)
+marker.bindPopup("ZICE Solutions").openPopup()
